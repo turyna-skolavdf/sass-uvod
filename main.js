7059d1aa36047d744eeb0685588fe7df9431b844
Miloslav Turyna <milan@turyna.eu>
2024-04-15 12:33:37 +0200

    *

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const display = document.querySelector('.display');
+const buttons = [...document.querySelectorAll('.btn')];
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.classList.contains('operator')) {
+            display.value += e.target.value;
+        } else if(e.target.classList.contains('equals')) {
+            try {
+                display.value = eval(display.value);
+            } catch {
+                display.value = "Chyba";
+            }
+        } else {
+            display.value += e.target.value;
+        }
+    });
+});
+
+document.querySelector('.clear').addEventListener('click', function() {
+    display.value = '';
+});
